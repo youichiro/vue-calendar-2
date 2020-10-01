b6ede97161799668c9302c4143f3f477469f4b71
youichiro <cinnamon416@gmail.com>
2020-10-01 23:12:52 +0900

    schema optionを追加

--- backend/db/migrate/20200930221132_create_events.rb
+++ backend/db/migrate/20200930221132_create_events.rb
@@ -1,10 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.string :name
-      t.datetime :start
-      t.datetime :end
-      t.boolean :timed
+      t.string :name, limit: 100, null: false
+      t.datetime :start, null: false
+      t.datetime :end, null: false
+      t.boolean :timed, default: true
       t.text :description
       t.string :color
 

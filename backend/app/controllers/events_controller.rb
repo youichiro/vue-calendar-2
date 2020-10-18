9745ad90c0461d42f1ec4dd76f3be11cccc44a71
youichiro <cinnamon416@gmail.com>
2020-10-18 10:38:24 +0900

    fix

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -34,6 +34,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
+    params.require(:event).permit(:name, :start, :end, :timed, :description, :color)
   end
 end

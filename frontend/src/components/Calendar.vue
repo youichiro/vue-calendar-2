06b77cf84a3d3cd2b48ad81ec3947f13f6536914
youichiro <cinnamon416@gmail.com>
2020-11-18 16:23:20 +0900

    axios.getでイベントデータを取得する

---
+++ frontend/src/components/Calendar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>events:</p>
+    <p>{{ events }}</p>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Calendar',
+  data: () => ({
+    events: []
+  }),
+  methods: {
+    fetchEvents() {
+      axios.get('http://localhost:3000/events')
+        .then((response) => {
+          this.events = response.data;
+        })
+        .catch((error) => {
+          console.erorr(error);
+        })
+    }
+  }
+};
+</script>

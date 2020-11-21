e23b99e4b9d46a5fb477e549871ffac85e1f50e2
youichiro <cinnamon416@gmail.com>
2020-11-21 21:09:11 +0900

    fix

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -17,13 +17,14 @@ export default {
   }),
   methods: {
     fetchEvents() {
-      axios.get('http://localhost:3000/events')
-        .then((response) => {
+      axios
+        .get('http://localhost:3000/events')
+        .then(response => {
           this.events = response.data;
         })
-        .catch((error) => {
+        .catch(error => {
           console.error(error);
-        })
+        });
     }
   }
 };

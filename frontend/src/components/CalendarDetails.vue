a8d83e697cb604ef248bd34691fe09ab030ffc1c
youichiro <cinnamon416@gmail.com>
2020-12-20 01:01:03 +0900

    CalendarDetailsコンポーネントを作成

---
+++ frontend/src/components/CalendarDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>CalendarDetails</h1>
+    <div v-for="event in events" :key="event.id">
+      <h2>{{ event.name }}</h2>
+      <p>{{ event.start }}</p>
+      <p>{{ event.end }}</p>
+      <p>{{ event.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarDetails',
+  data: () => ({
+    events: [],
+  }),
+  methods: {
+    fetchEvents() {
+      axios
+        .get('http://localhost:3000/events')
+        .then((response) => {
+          this.events = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>

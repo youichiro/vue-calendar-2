17a8b017dac0bc9bd8f65f87a7113c7926bb619b
youichiro <cinnamon416@gmail.com>
2021-02-06 12:47:37 +0900

    月を指定してカレンダーを表示する

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <v-sheet height="100vh">
-      <v-calendar></v-calendar>
+      <v-calendar
+        v-model="value"
+      ></v-calendar>
     </v-sheet>
   </div>
 </template>
@@ -11,6 +13,9 @@ import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Calendar',
+  data: () => ({
+    value: new Date('2020-10-01') // 表示する月を指定
+  }),
   computed: {
     ...mapGetters('events', ['events'])
   },

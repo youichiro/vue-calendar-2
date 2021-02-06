aa2ae7a26d2b63c08671713cc8cd2fe7113a48bc
youichiro <cinnamon416@gmail.com>
2021-02-06 14:33:25 +0900

    年月を表示する

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <v-sheet height="100vh">
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-toolbar-title>2020年 10月</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
       <v-calendar
         v-model="value"
         :events="events"
@@ -19,7 +22,7 @@ export default {
     value: new Date('2020-10-01') // 表示する月を指定
   }),
   computed: {
-    ...mapGetters('events', ['events'])
+    ...mapGetters('events', ['events']),
   },
   methods: {
     ...mapActions('events', ['fetchEvents'])

630d92669f6948ec009471b6adfbc23d412a8fa8
youichiro <cinnamon416@gmail.com>
2021-02-14 16:31:19 +0900

    prettier

--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -4,22 +4,26 @@ const apiUrl = 'http://localhost:3000';
 
 const state = {
   events: [],
-  event: null,
+  event: null
 };
 
 const getters = {
-  events: state => state.events.map(event => {
-    return {
-      ...event,
-      start: new Date(event.start),
-      end: new Date(event.end)
-    }
-  }),
-  event: state => state.event ? {
-    ...state.event,
-    start: new Date(state.event.start),
-    end: new Date(state.event.end)
-  } : null
+  events: state =>
+    state.events.map(event => {
+      return {
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end)
+      };
+    }),
+  event: state =>
+    state.event
+      ? {
+          ...state.event,
+          start: new Date(state.event.start),
+          end: new Date(state.event.end)
+        }
+      : null
 };
 
 const mutations = {

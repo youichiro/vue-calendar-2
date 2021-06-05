dfe9c89d989a95198b52583c1f62fe3f07d8728c
youichiro <cinnamon416@gmail.com>
2021-06-05 14:11:31 +0900

    rails generate model calendar name:string color:string visibility:boolean

---
+++ backend/db/migrate/20210605045701_create_calendars.rb
@@ -0,0 +1,11 @@
+class CreateCalendars < ActiveRecord::Migration[6.0]
+  def change
+    create_table :calendars do |t|
+      t.string :name
+      t.string :color
+      t.boolean :visibility
+
+      t.timestamps
+    end
+  end
+end

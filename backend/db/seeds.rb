4eed96df9b1ddb96d462660b31e83add20cc2a1d
youichiro <cinnamon416@gmail.com>
2021-06-06 16:24:59 +0900

    fix seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,6 +1,7 @@
-Event.create!(name: '予定1', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00')
-Event.create!(name: '予定2', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00')
-Event.create!(name: '予定3', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00')
-Event.create!(name: '予定4', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00')
+calendar = Calendar.create!(name: '予定')
 
-Calendar.create!(name: '予定')
+Event.create!(name: '予定1', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00', calendar: calendar)
+Event.create!(name: '予定2', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00', calendar: calendar)
+Event.create!(name: '予定3', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00', calendar: calendar)
+Event.create!(name: '予定4', start: '2020-10-27 10:00:00', end: '2020-10-27 11:00:00', calendar: calendar)
+

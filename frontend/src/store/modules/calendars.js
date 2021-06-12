0f58c2ee42d362c7e63f9733e4e1df3579fca203
youichiro <cinnamon416@gmail.com>
2021-06-12 15:18:49 +0900

    Vuexストアにカレンダー更新・削除アクションを追加

--- frontend/src/store/modules/calendars.js
+++ frontend/src/store/modules/calendars.js
@@ -16,6 +16,9 @@ const getters = {
 const mutations = {
   setCalendars: (state, calendars) => (state.calendars = calendars),
   appendCalendar: (state, calendar) => (state.calendars = [...state.calendars, calendar]),
+  updateCalendar: (state, calendar) =>
+    (state.calendars = state.calendars.map(c => (c.id === calendar.id ? calendar : c))),
+  removeCalendar: (state, calendar) => (state.calendars = state.calendars.filter(c => c.id !== calendar.id)),
   setCalendar: (state, calendar) => (state.calendar = calendar),
 };
 
@@ -27,6 +30,14 @@ const actions = {
   async createCalendar({ commit }, calendar) {
     const response = await axios.post(`${apiUrl}/calendars`, calendar);
     commit('appendCalendar', response.data);
+  },
+  async updateCalendar({ commit }, calendar) {
+    const response = await axios.put(`${apiUrl}/calendars/${calendar.id}`, calendar);
+    commit('updateCalendar', response.data);
+  },
+  async deleteCalendar({ commit }, id) {
+    const response = await axios.delete(`${apiUrl}/calendars/${id}`);
+    commit('removeCalendar', response.data);
   },
   setCalendar({ commit }, calendar) {
     commit('setCalendar', calendar);

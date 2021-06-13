bdad52e97ed03dded786150d0e918fa294133d74
youichiro <cinnamon416@gmail.com>
2021-06-13 11:38:35 +0900

    終日の予定は開始・終了時刻を書き換える

--- frontend/src/functions/serializers.js
+++ frontend/src/functions/serializers.js
@@ -1,11 +1,17 @@
-import { format } from 'date-fns';
+import { format, set } from 'date-fns';
 
 export const serializeEvent = event => {
   if (event === null) {
     return null;
   }
-  const start = new Date(event.start);
-  const end = new Date(event.end);
+
+  let start = new Date(event.start);
+  let end = new Date(event.end);
+  if (!event.timed) {
+    start = set(start, { hours: 0, minutes: 0, seconds: 0 });
+    end = set(end, { hours: 23, minutes: 59, seconds: 59 });
+  }
+
   return {
     ...event,
     start,
